775d57eaf783613aa6f7de1f820722882741be72
Betsy-it-Llc <betsyit.lalachante@gmail.com>
2024-01-05 16:58:56 -0400

    configuration de la navigation

--- src/screens/Route.jsx
+++ src/screens/Route.jsx
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import Home from './Home'
+import Players from './Players'
+import Category from './Category'
+import TruthOrDare from './TruthOrDare'
+import ShowTruthOrDare from './ShowTruthOrDare'
+
+const Stack = createNativeStackNavigator()
 
 const Route = () => {
   return (
-    <View>
-      <Text>Route</Text>
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name='Home' component={Home}/>
+            <Stack.Screen name='Players' component={Players}/>
+            <Stack.Screen name='Category' component={Category}/>
+            <Stack.Screen name='TruthOrDare' component={TruthOrDare}/>
+            <Stack.Screen name='ShowTruthOrDare' component={ShowTruthOrDare}/>
+        </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
 export default Route
+
