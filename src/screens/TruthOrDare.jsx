4582a8301f2f9cd42c226aa34fc1ad171e86573f
Betsy-it-Llc <betsyit.lalachante@gmail.com>
2024-01-08 11:47:48 -0400

    maj des boutons

--- src/screens/TruthOrDare.jsx
+++ src/screens/TruthOrDare.jsx
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Button } from 'react-native-paper'
 
-const TruthOrDare = () => {
+const TruthOrDare = ({navigation}) => {
   return (
     <View>
       <Text>TruthOrDare</Text>
+      <Button onPress={()=>navigation.navigate('ShowTruthOrDare')}>ACTION</Button>
+      <Button onPress={()=>navigation.navigate('ShowTruthOrDare')}>VÉRITÉ</Button>
     </View>
   )
 }
