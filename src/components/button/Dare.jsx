26901bd191624dce7d0ff4a6c7ddc60ee1a638e7
Betsy-it-Llc <betsyit.lalachante@gmail.com>
2024-01-08 11:15:39 -0400

    réalisation du dossier button avec les boutons action et vérité

---
+++ src/components/button/Dare.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Dare = () => {
+  return (
+    <View>
+      <Text>Dare</Text>
+    </View>
+  )
+}
+
+export default Dare
