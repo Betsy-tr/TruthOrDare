78efb050b829bd1ed4e4cfee99ec7cb10efca240
Betsy-it-Llc <betsyit.lalachante@gmail.com>
2024-01-08 11:36:24 -0400

    intéraction des boutons

--- src/components/button/Dare.jsx
+++ src/components/button/Dare.jsx
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
+import { Button } from 'react-native-paper'
 
-const Dare = () => {
+const Dare = ({navigation}) => {
   return (
     <View>
-      <Text>Dare</Text>
+      <Button onPress={()=>navigation.navigate('ShowTruthOrDare')}>ACTION</Button>
     </View>
   )
 }
