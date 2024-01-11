329663e5e8c0e123ac376c874751a4da1598b1b6
Betsy-it-Llc <betsyit.lalachante@gmail.com>
2024-01-11 19:18:42 -0400

    fonctionnalité de player.jsx

--- src/screens/Players.jsx
+++ src/screens/Players.jsx
@@ -1,11 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import StartGame from '../components/game/StartGame'
+import ItemPlayer from '../components/player/ItemPlayer'
+import EmptyPlayer from '../components/player/EmptyPlayer'
+import AddPlayer from '../components/player/AddPlayer'
+import ResetPlayer from '../components/player/ResetPlayer'
+import { useSelector } from 'react-redux'
 
 const Players = () => {
+
+  const { players } = useSelector(state=>state.player)
+
   return (
     <View>
       <Text>Players</Text>
+      <FlatList
+        data={players}
+        renderItem={({item})=><ItemPlayer player={item}/>}
+        keyExtractor={item=>item.id}
+        ListEmptyComponent={EmptyPlayer}
+        ListHeaderComponent={AddPlayer}
+      />
+      <ResetPlayer/>
       <StartGame/>
     </View>
   )
