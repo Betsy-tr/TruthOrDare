ab6ea35dec15327afcf9fdbe2e14970d5bf151f0
Betsy-it-Llc <betsyit.lalachante@gmail.com>
2024-01-15 12:20:08 -0400

    Fonctionnalité des composants

--- src/screens/ShowTruthOrDare.jsx
+++ src/screens/ShowTruthOrDare.jsx
@@ -1,11 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import ResetGame from '../components/game/ResetGame'
+import { useSelector } from 'react-redux'
+import NextPlayer from '../components/player/NextPlayer'
 
 const ShowTruthOrDare = () => {
+
+  const { players , position } = useSelector(state=>state.player)
+
   return (
     <View>
       <Text>ShowTruthOrDare</Text>
+      <Text>{players[position].name}</Text>
+      <NextPlayer/>
       <ResetGame/>
     </View>
   )
--- src/screens/TruthOrDare.jsx
+++ src/screens/TruthOrDare.jsx
@@ -1,11 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Button } from 'react-native-paper'
+import { useSelector } from 'react-redux'
 
 const TruthOrDare = ({navigation}) => {
+
+  const { players , position } = useSelector(state=>state.player)
+
   return (
     <View>
       <Text>TruthOrDare</Text>
+      <Text>{players[position].name}</Text>
       <Button onPress={()=>navigation.navigate('ShowTruthOrDare')}>ACTION</Button>
       <Button onPress={()=>navigation.navigate('ShowTruthOrDare')}>VÉRITÉ</Button>
     </View>
