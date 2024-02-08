7bca79958729d614a42353a4111a5f0a5dc42420
Betsy-it-Llc <betsyit.lalachante@gmail.com>
2024-02-08 12:12:47 -0400

    ajout de la condition du bouton startGame

--- src/components/game/StartGame.jsx
+++ src/components/game/StartGame.jsx
@@ -2,19 +2,22 @@ import { View } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { Button } from 'react-native-paper'
+import { useSelector } from 'react-redux'
 
 const StartGame = () => {
 
     const navigation = useNavigation()
 
+    const { players } = useSelector(state=>state.player)
+
     const start = () => {
 
-        navigation.navigate('Category')
+      navigation.navigate('Category')
     }
 
   return (
     <View>
-      <Button onPress={start}>Suivant</Button>
+      <Button disabled={!(players.length >= 2)} onPress={start}>Suivant</Button>
     </View>
   )
 }
