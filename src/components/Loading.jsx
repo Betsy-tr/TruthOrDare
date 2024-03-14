42ff7428cbf4686f75aff86e06d369800fc1d8fe
Betsy-it-Llc <betsyit.lalachante@gmail.com>
2024-03-14 14:19:26 -0400

    style loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,13 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { ActivityIndicator } from 'react-native-paper'
+import { ActivityIndicator , MD2Colors} from 'react-native-paper'
 
 
 
 const Loading = () => {
   return (
 
-    <ActivityIndicator animating={true} style={{color : '#09D0EB' }} size='large' />
+    <ActivityIndicator animating={true} color={MD2Colors.blue300} style={{ marginTop : 250}} size={130} />
 
   )
 }
